83970050cd9fdad8bd4253dd754dc30b920b4048
samdovon <samdovon@gmail.com>
2024-01-14 14:53:01 +0700

    change directory workflows

--- myapp.py
+++ myapp.py
@@ -2,13 +2,3 @@ import pandas as pd
 import pytest
 print("Hello CICD")
 # Extract Function
-
-
-def readdf():
-    data = [['tom', 10], ['nick', 15], ['juli', 14], ['tom', 10]]
-    df = pd.DataFrame(data, columns=['Name', 'Age'])
-    return df
-
-
-df = readdf()
-print(df.columns)
--- test_myapp.py
+++ test_myapp.py
@@ -1,8 +1,12 @@
 import pandas as pd
 from myapp import *
 
-df = readdf()
 
+@pytest.fixture
+def readdf():
+    data = [['tom', 10], ['nick', 15], ['juli', 14], ['tom', 10]]
+    df = pd.DataFrame(data, columns=['Name', 'Age'])
+    return df
 
 def test_col_exists(df):
     cols = ['Name', 'Age']
