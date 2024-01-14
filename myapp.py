8e705827abb68577703f98a24833aed93a2d946e
samdovon <samdovon@gmail.com>
2024-01-14 12:29:48 +0700

    Adding Virtual Venv and Update ETL code

--- myapp.py
+++ myapp.py
@@ -1,1 +1,14 @@
+import pandas as pd
 print("Hello CICD")
+# Extract Function
+def readdf():
+    data = [['tom', 10], ['nick', 15], ['juli', 14],['tom', 10]]
+    df = pd.DataFrame(data, columns=['Name', 'Age'])
+    return df
+# Transform Function
+def dedup(df,col)
+    df.drop_duplicates(subset=col)
+    return df
+# Load Function
+def 
+# Execute Step
